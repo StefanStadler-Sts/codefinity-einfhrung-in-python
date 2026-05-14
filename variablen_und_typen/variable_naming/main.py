d19d527fbe6041c299e398bb11054a89b18a74fe
StefanStadler-Sts <>
2026-05-14 13:23:23 +0000

    Variable Naming

---
+++ variablen_und_typen/variable_naming/main.py
@@ -0,0 +1,12 @@
+item_name = "Bread"
+item_price = 2.50
+item_quantity =250
+
+
+# Testing
+print("Item:", item_name)
+print("Price per loaf: $", item_price)
+print("Quantity in stock:", item_quantity)
+
+
+
